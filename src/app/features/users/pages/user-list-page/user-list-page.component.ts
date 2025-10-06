1ddce0a1ebe34444b099de92037642b3d0144580
MAlameh <mmwalameh@gmail.com>
2025-10-07 00:14:13 +0300

    feat(users) : integrate the custom generic list into UserListPage

--- src/app/features/users/pages/user-list-page/user-list-page.component.ts
+++ src/app/features/users/pages/user-list-page/user-list-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../../core/services/api.service';
+import { ApiURL } from '../../../../core/miscellaneous/api.template';
+import { UserService } from '../../../../core/services/user.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-list-page',
   templateUrl: './user-list-page.component.html',
   styleUrl: './user-list-page.component.scss'
 })
-export class UserListPageComponent {
+export class UserListPageComponent implements OnInit {
+  users = [];
+  usersEntityApiUrl = ApiURL.users;
+  usersEntityApiHeader = new HttpHeaders();
+  searchTerm = ''
 
+  constructor(private userService: UserService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.usersEntityApiHeader = this.userService.headers;
+  }
+  resetSearch() {
+    this.searchTerm = '';
+  }
 }
--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -4,14 +4,32 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { UserDetailsPageComponent } from './pages/user-details-page/user-details-page.component';
 import { UserListPageComponent } from './pages/user-list-page/user-list-page.component';
-
-
+import { CustomListComponent } from '../../shared/custom-list/custom-list.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  declarations: [UserListPageComponent,UserDetailsPageComponent],
+  declarations: [
+    UserListPageComponent, UserDetailsPageComponent
+  ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    CustomListComponent,
+    InputTextModule,
+    FormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    CardModule
   ]
 })
 export class UsersModule { }
